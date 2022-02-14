adb2ff6a932ab8139dd7fc44f503fe5291b0c27e
bbnp1980 <97748924+bbnp1980@users.noreply.github.com>
2022-02-14 20:54:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-
 resource "aws_s3_bucket" "b" {
   bucket = "myawsbucket12345554555"
   acl    = "private"
@@ -15,4 +14,5 @@ resource "aws_s3_bucket" "b" {
 }
 
 variable "region" {
-  
+
+}
